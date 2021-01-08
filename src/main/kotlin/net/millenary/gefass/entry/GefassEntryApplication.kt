9ac269a06917d978cc56420b3f94b31af82c9c34
縣 <ymgtech@millenary.net>
2021-01-08 23:36:47 +0900

    [init] Spring Initializr でプロジェクトを初期化

---
+++ src/main/kotlin/net/millenary/gefass/entry/GefassEntryApplication.kt
@@ -0,0 +1,11 @@
+package net.millenary.gefass.entry
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GefassEntryApplication
+
+fun main(args: Array<String>) {
+	runApplication<GefassEntryApplication>(*args)
+}
---
+++ src/test/kotlin/net/millenary/gefass/entry/GefassEntryApplicationTests.kt
@@ -0,0 +1,13 @@
+package net.millenary.gefass.entry
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GefassEntryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
